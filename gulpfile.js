b603a6cf512a16ba6d277c88e5174469480d83e8
Ahmad Mahmoud <ahmad.mahmoud@gmail.com>
2018-10-28 13:20:16 -0700

    Glup Watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,37 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var browserSync = require("browser-sync");
+
+var SourePath = {
+  sassPath: "src/scss/*.scss"
+};
+
+var AppPath = {
+  root: "app",
+  css: "app/css",
+  js: "app/js"
+};
 
 gulp.task("sass", function() {
   return gulp
-    .src("src/scss/app.scss")
+    .src(SourePath.sassPath)
     .pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError))
-    .pipe(gulp.dest("app/css"));
+    .pipe(gulp.dest(AppPath.css));
 });
 
-gulp.task("default", ["sass"]);
+gulp.task("serve", function() {
+  browserSync.init(
+    [AppPath.css + "/*.css", AppPath.root + "/*.html", AppPath.js + "/*.js"],
+    {
+      server: {
+        baseDir: AppPath.root
+      }
+    }
+  );
+});
+
+gulp.task("watch", ["sass", "serve"], function() {
+  gulp.watch([SourePath.sassPath], ["sass"]);
+});
+
+gulp.task("default", ["watch"]);
